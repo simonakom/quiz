e3c6f56322e3cf8834423abd52f05fd015de24ed
simonakomarovska <simonak399@gmail.com>
2024-04-22 17:39:15 +0200

    fixed js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+let answersChecked = false; 
+
 function markSelectedAnswer(questionId, selectedAnswerId) {
+    if (answersChecked) return; 
+
     // Remove 'selected' class from all answer options for the given question
     let answerOptions = document.querySelectorAll(`#${questionId} .answer-option`);
     answerOptions.forEach(option => option.classList.remove('selected'));
@@ -9,7 +13,22 @@ function markSelectedAnswer(questionId, selectedAnswerId) {
     }
 }
 
+function allQuestionsAnswered() {
+    let answeredQuestions = document.querySelectorAll('.answer-option.selected').length;
+    return answeredQuestions === 5; 
+}
+
+
 function calculateScore() {
+    let score = document.getElementById('result');
+
+    if (!allQuestionsAnswered()) {
+        score.innerText = ('Please answer all questions before submitting!');
+        return;
+    }
+
+    answersChecked = true;
+
     // Count total Score
     let correctAnswers = 0;
 
@@ -29,40 +48,32 @@ function calculateScore() {
         correctAnswers++;
     }
 
-    let score = document.getElementById('result');
     if (correctAnswers === 5) {
         score.innerHTML += "<br><span style='color: #ea226c; font-size: 20px;'>Congratulations! You answered all questions correctly! 🥳</span>";
     } else {
         score.innerText = `Your Score: ${correctAnswers} out of 5. You can do better! 😓`
     }
 
-    //check if all selected
-    let answeredQuestions = document.querySelectorAll('.answer-option.selected').length;
-    if (answeredQuestions < 5) {
-        score.innerText = ('Please answer all questions before submitting!');
-        return;
-    }
-
     //--------------------------------Display correct answers---------------------------------------------
     // 1 question
-    let selectOne = document.querySelector('#paris');
-    let selectTwo = document.querySelector('#london');
-    let selectThree = document.querySelector('#berlin');
+    let select1 = document.querySelector('#paris');
+    let select2 = document.querySelector('#london');
+    let select3 = document.querySelector('#berlin');
 
     let displayAnswerOne = document.querySelector('#displayCorrectAnswer1');
     if (document.querySelector('#paris').classList.contains('selected')) {
         displayAnswerOne.innerText = `You are right! 👏`;
         displayAnswerOne.style.color = '#309a53d2'; 
-        selectOne.classList.add('selected2');
+        select1.classList.add('selectedCorrect');
     }else if (document.querySelector('#london').classList.contains('selected')) {
-        selectTwo.classList.add('selected3');
-        displayAnswerOne.innerText = `Correct Answer: C) Paris! 😢`;
+        select2.classList.add('selectedWrong');
+        displayAnswerOne.innerText = `Correct Answer: C)! 😢`;
     }
     else if (document.querySelector('#berlin').classList.contains('selected')) {
-        selectThree.classList.add('selected3');
-        displayAnswerOne.innerText = `Correct Answer: C) Paris! 😢`;
+        select3.classList.add('selectedWrong');
+        displayAnswerOne.innerText = `Correct Answer: C)! 😢`;
     }else {
-        displayAnswerOne.innerText = `Correct answer was C) Paris! 😢`;
+        displayAnswerOne.innerText = `Correct answer was C)! 😢`;
     }
                         
     // 2 question
@@ -74,16 +85,16 @@ function calculateScore() {
     if (document.querySelector('#seven').classList.contains('selected')) {
         displayAnswerTwo.innerText = `You are right! 👏`;
         displayAnswerTwo.style.color = '#309a53d2'; 
-        select22.classList.add('selected2');
+        select22.classList.add('selectedCorrect');
     }else if (document.querySelector('#five').classList.contains('selected')) {
-        select11.classList.add('selected3');
-        displayAnswerTwo.innerText = `Correct Answer: B) Seven! 😢`;
+        select11.classList.add('selectedWrong');
+        displayAnswerTwo.innerText = `Correct Answer: B)! 😢`;
     }
     else if (document.querySelector('#six').classList.contains('selected')) {
-        select33.classList.add('selected3');
-        displayAnswerTwo.innerText = `Correct Answer: B) Seven! 😢`;
+        select33.classList.add('selectedWrong');
+        displayAnswerTwo.innerText = `Correct Answer: B)! 😢`;
     }else {
-        displayAnswerTwo.innerText = `Correct answer was B) Seven! 😢`;
+        displayAnswerTwo.innerText = `Correct answer was B)! 😢`;
     }
 
     // 3 question
@@ -95,16 +106,16 @@ function calculateScore() {
     if (document.querySelector('#mars').classList.contains('selected')) {
         displayAnswerThree.innerText = `You are right! 👏`;
         displayAnswerThree.style.color = '#309a53d2'; 
-        select111.classList.add('selected2');
+        select111.classList.add('selectedCorrect');
     }else if (document.querySelector('#jupiter').classList.contains('selected')) {
-        select222.classList.add('selected3');
-        displayAnswerThree.innerText = `Correct Answer: A) Mars! 😢`;
+        select222.classList.add('selectedWrong');
+        displayAnswerThree.innerText = `Correct Answer: A)! 😢`;
     }
     else if (document.querySelector('#saturn').classList.contains('selected')) {
-        select333.classList.add('selected3');
-        displayAnswerThree.innerText = `Correct Answer: A) Mars! 😢`;
+        select333.classList.add('selectedWrong');
+        displayAnswerThree.innerText = `Correct Answer: A)! 😢`;
     }else {
-        displayAnswerThree.innerText = `Correct answer was A) Mars! 😢`;
+        displayAnswerThree.innerText = `Correct answer was A)! 😢`;
     }
 
     // 4 question
@@ -116,18 +127,16 @@ function calculateScore() {
     if (document.querySelector('#dioxide').classList.contains('selected')) {
         displayAnswerFour.innerText = `You are right! 👏`;
         displayAnswerFour.style.color = '#309a53d2'; 
-        select2222.classList.add('selected2');
+        select2222.classList.add('selectedCorrect');
     }else if (document.querySelector('#oxygen').classList.contains('selected')) {
-        select1111.classList.add('selected3');
-        displayAnswerFour.innerText = `Correct Answer:\n B) Carbon Dioxide
-        ! 😢`;
+        select1111.classList.add('selectedWrong');
+        displayAnswerFour.innerText = `Correct Answer: B)! 😢`;
     }
     else if (document.querySelector('#nitrogen').classList.contains('selected')) {
-        select3333.classList.add('selected3');
-        displayAnswerFour.innerText = `Correct Answer:\n B) Carbon Dioxide
-        ! 😢`;
+        select3333.classList.add('selectedWrong');
+        displayAnswerFour.innerText = `Correct Answer: B)! 😢`;
     }else {
-        displayAnswerFour.innerText = `Correct answer was B) Carbon Dioxide! 😢`;
+        displayAnswerFour.innerText = `Correct answer was B)! 😢`;
     }
 
     // 5 question
@@ -139,28 +148,24 @@ function calculateScore() {
     if (document.querySelector('#whale').classList.contains('selected')) {
         displayAnswerFive.innerText = `You are right! 👏`;
         displayAnswerFive.style.color = '#309a53d2'; 
-        select22222.classList.add('selected2');
+        select22222.classList.add('selectedCorrect');
     }else if (document.querySelector('#elephant').classList.contains('selected')) {
-        select11111.classList.add('selected3');
-        displayAnswerFive.innerText = `Correct Answer: B) Blue Whale
-        ! 😢`;
+        select11111.classList.add('selectedWrong');
+        displayAnswerFive.innerText = `Correct Answer: B)! 😢`;
     }
     else if (document.querySelector('#giraffe').classList.contains('selected')) {
-        select33333.classList.add('selected3');
-        displayAnswerFive.innerText = `Correct Answer:\n B) Blue Whale
-        ! 😢`;
+        select33333.classList.add('selectedWrong');
+        displayAnswerFive.innerText = `Correct Answer: B)! 😢`;
     }else {
-        displayAnswerFive.innerText = `Correct answer was B) Blue Whale! 😢`;
+        displayAnswerFive.innerText = `Correct answer was B)! 😢`;
     }
 
-    document.querySelector('#hide').onclick = playAgain;
-    document.querySelector('#hide').innerHTML = 'Play Again!';
+    document.querySelector('#checkButton').onclick = playAgain;
+    document.querySelector('#checkButton').innerHTML = 'Try Again!';
 }
 
 function playAgain() {
     location.reload();
-    let button =  document.querySelector('#hide');
-    button.innerText = `Play Again`;
   }
 
 
